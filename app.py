81e3c545f1375158dacb5c83926392f8d3f1bff2
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-15 17:45:05 -0300

    Implementando rota de adicionar produto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from repository.database import db
 
@@ -14,6 +14,21 @@ class Product(db.Model):
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=False)
+
+
+@app.route("/api/products/add", methods=["POST"])
+def add_product():
+    data = request.json
+    if "name" in data and "price" in data:
+        product = Product(
+            name=data["name"],
+            price=data["price"],
+            description=data.get("description", ""),
+        )
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
 
 
 @app.route("/")
