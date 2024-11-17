480b8aa761e0d53a4d9cc513c78368a0c83ceea8
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-17 18:32:11 -0300

    Implementando rota PUT

--- app.py
+++ app.py
@@ -56,6 +56,26 @@ def get_product_details(product_id):
     return jsonify({"message": "Product not found"}), 404
 
 
+@app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+    data = request.json
+    if "name" in data:
+        product.name = data["name"]
+
+    if "price" in data:
+        product.price = data["price"]
+
+    if "description" in data:
+        product.description = data["description"]
+
+    db.session.commit()
+
+    return jsonify({"message": "Product updated sucessfully"}), 200
+
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
