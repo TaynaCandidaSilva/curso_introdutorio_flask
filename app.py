f80c19fa2876220f1b1a823168ecc5a09079fa85
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-19 14:16:34 -0300

    Implementando rotas de login, logout e inserindo autenticacao

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from repository.database import db
 from flask_cors import CORS
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "minha_chave_123"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"  # Configuracao do B.D
 
+login_manager = LoginManager()
 # Iniciando o Banco de Dados
 db = SQLAlchemy(app)
+
+# Logar usuario
+login_manager.init_app(app)
+login_manager.login_view = "login"
 CORS(app)
 
 
@@ -25,7 +31,33 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=False)
 
 
+# Autenticacao
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+
+    user = User.query.filter_by(username=data["username"]).first()
+
+    if user and data.get("password") == user.password:
+        login_user(user)
+        return jsonify({"message": "Logged in sucessfully"}), 200
+    return jsonify({"message": "Unauthorized. Invalid credentials"}), 401
+
+
+@app.route("/logout", methods=["POST"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout sucessfully"}), 200
+
+
 @app.route("/api/products/add", methods=["POST"])
+@login_required
 def add_product():
     data = request.json
     if "name" in data and "price" in data:
@@ -41,6 +73,7 @@ def add_product():
 
 
 @app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
+@login_required
 def delete_product(product_id):
     product = Product.query.get(product_id)
     if product:
@@ -66,6 +99,7 @@ def get_product_details(product_id):
 
 
 @app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+@login_required
 def update_product(product_id):
     product = Product.query.get(product_id)
     if not product:
