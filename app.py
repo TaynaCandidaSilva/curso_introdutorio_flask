e52a4b0dd8218211203bed6f7eea1ee634477270
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-11-21 14:19:11 -0300

    Implementando rota de checkout de produtos do carrinho

--- app.py
+++ app.py
@@ -196,5 +196,16 @@ def view_cart():
     return jsonify(cart_content)
 
 
+@app.route("/api/cart/checkout", methods=["POST"])
+@login_required
+def checkout():
+    user = User.query.get(int(current_user.id))
+    cart_items = user.cart
+    for cart_item in cart_items:
+        db.session.delete(cart_item)
+    db.session.commit()
+    return jsonify({"message": "Checkout successful. Cart has been cleared"}), 200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
